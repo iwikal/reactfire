0237250102dc783b1c71dd7ae4d1d39fdbfc425f
Joel Nordström <e.joel.nordstrom@gmail.com>
2020-03-31 23:47:38 +0200

    Add firebase to scope

--- reactfire/src/auth/index.tsx
+++ reactfire/src/auth/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import { user } from 'rxfire/auth';
 import { Resource } from '../resource';
 import { useObservable } from '../useObservable';
